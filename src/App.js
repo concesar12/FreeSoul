70364b6ca55f960bed7764caeb71bb1d68db1cf2
AndresFSalcedo <84711845+AndresFSalcedo@users.noreply.github.com>
2021-08-27 16:43:27 +1200

    First Update
    Header + NavTab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
+import React from 'react';
 import './App.css';
-import Slider from './Components/Slider/Slider'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Slider from './Components/Slider/Slider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TabNav from './Components/Navbar/TabNav';
+import Tab from './Components/Navbar/Tab';
 
-function App() {
-  return (
-    <>
-      
-      <Slider />
-    </>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      selected: 'Home'
+    }
+  }
+
+  setSelected = (tab) => {
+    this.setState({selected: tab});
+  }
+
+  render() {
+    return (
+      <>
+        <TabNav tabs={['Home','Camisetas','Buzos','Blog','Nosotras']} selected={this.state.selected} setSelected={this.setSelected}>
+          <Tab isSelected={this.state.selected === 'Home'}>
+            <h1 className="topMessage mt-5">¡Siéntete poderosa con nuestras prendas!</h1>
+            <Slider />
+            <p>Home</p>
+          </Tab>
+          <Tab isSelected={this.state.selected === 'Camisetas'}>
+            <p>Camisetas</p>
+          </Tab>
+          <Tab isSelected={this.state.selected === 'Buzos'}>
+            <p>Buzos</p>
+          </Tab>
+          <Tab isSelected={this.state.selected === 'Blog'}>
+            <p>Blog</p>
+          </Tab>
+          <Tab isSelected={this.state.selected === 'Nosotras'}>
+            <p>Nosotras</p>
+          </Tab>
+        </TabNav>
+        
+      </>
+    );
+  }
+
 }
 
 export default App;
--- src/Components/Navbar/TabNav.js
+++ src/Components/Navbar/TabNav.js
@@ -1,16 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import './TabNav.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaWhatsapp} from 'react-icons/fa';
+import {FaTruck} from 'react-icons/fa';
 
 class TabNav extends React.Component {
 
     render(){
         return(
-            <div style={{width:'30%'}}>
-                <ul className="nav nav-tabs">
-                    {
-                        this.props.tabs.map()
-                    }
-                </ul>
-                {this.props.children}
+            <div>
+                <div className="top-bar topBar">
+                    <div className="container">
+                        <div className="col-12">
+                            <p>
+                                Envíos a toda Colombia
+                                <a className="topLink" href="tel:+64225946488"><FaWhatsapp/> Contáctanos</a>
+                                <a className="topLink" href="https://envia.co/" target="_blank" rel="noreferrer"><FaTruck/> Sigue tu pedido</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <ul className="nav nav-tabs justify-content-center tabcss">
+                        {
+                            this.props.tabs.map(tab => {
+                                const active =(tab === this.props.selected ? 'active' : '');
+                                return(
+                                    <li className="nav-item" key={tab}>
+                                        <a href="#" className={"nav-link " + active} onClick={ () => this.props.setSelected(tab)}>
+                                            {tab}
+                                        </a>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                    {this.props.children}
+                </div>
             </div>
         )
     }
